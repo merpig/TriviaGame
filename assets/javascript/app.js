751efd4d2b1f603e8b128048b2352ffebc65d3b0
Sasha Peters <peteram1@sewanee.edu>
2019-02-26 21:19:20 -0600

    end game functionality added

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,6 @@
+
 $(document).ready(function() {
+    //$(".btn-primary").addClass("hidden");
     var randomSet = [0,1,2,3];
 
     /***********Borrowed from stack overflow solutions********/
@@ -10,19 +12,79 @@ $(document).ready(function() {
     /**/    }
     /*********************************************************/
 
-    //var difficulty = "easy";
     var wins = 0;
     var losses = 0;
     var skips = 3;
     var points = 0;
     var difficulty = "easy";
-    
-    //var numDocs = parseInt($("#records").val());
-    generateQuiz();
-    // Ajex call to queryURL
+    var canPlay = true;
+    var round = 0;
+    var styleColor = "";
+    var time;
+
+    function timer(){
+        
+        var timeleft = 10;
+        time = setInterval(function(){
+            $(".timeLeft").text("Time left: " + timeleft);
+            timeleft -= 1;
+            if(timeleft < 0){
+                $("#play").prop("disabled", false);
+                canPlay = true;
+                clearInterval(time);
+                if(styleColor === "green") points-=1;
+                if(styleColor === "yellow") points-=2;
+                if(styleColor === "red") points-=3;
+                $(".gameControl").removeClass("panel-success");
+                $(".gameControl").removeClass("panel-warning");
+                $(".gameControl").addClass("panel-danger");
+                $(".gameMessages").text("Round " + round + ": You've run out of time!");
+                refreshData();
+            }
+        }, 1000);
+    }
+
+    function reset(){
+        clearInterval(time);
+
+        $(".gameControl").removeClass("panel-danger");
+        $(".gameControl").removeClass("panel-warning");
+        $(".gameControl").addClass("panel-success");
+        $(".gameMessages").text("Round 0: Game Reset");
+
+        $("#play").prop("disabled", false);
+        $("#skip").prop("disabled", false);
+        $(".question").empty();
+        $("#a0,#a1,#a2,#a3").empty();
+
+        wins = 0;
+        losses = 0;
+        skips = 3;
+        points = 0;
+        difficulty = "easy";
+        canPlay = true;
+        round = 0;
+        styleColor = "";
+        time;
+        refreshData();
+    }
+
+    function refreshData(){
+        $(".timeLeft").text("Time left: ");
+        $("#ws").text(wins);
+        $("#ls").text(losses);
+        $("#points").text(points);
+        $("#curdif").text(difficulty).css("color",styleColor);
+        $("#skip").text("Skip (" + skips + ")");
+        //answered = false;
+    }
+
     function generateQuiz(){
         difficulty = $("#records").val();
-        //console.log(difficulty);
+        if(difficulty==="easy") { styleColor= "green"; }
+        if(difficulty==="medium") { styleColor= "orange"; }
+        if(difficulty==="hard") { styleColor= "red"; }
+
         var queryURL = "https://opentdb.com/api.php?amount=1&difficulty="+ difficulty +"&type=multiple";
         $(".question").empty();
         $(".answer").empty();
@@ -39,7 +101,6 @@ $(document).ready(function() {
             for(var i = 0; i<3; i++){
                 var tempClass = "#a" + randomSet[i];
                 $(tempClass).text(tempArr[i]);
-                //console.log(tempClass + "   " + tempArr[i]);
             }
 
             var temp = "#a" + randomSet[3];
@@ -48,31 +109,120 @@ $(document).ready(function() {
         });
     }
 
-    $(".btn").on("click", function(){
-        if(this.id===String("b"+randomSet[3])) {
+    $("#play").on("click", function(){
+
+        if(canPlay && round < 10){
+
+            timer();
+            round++;
+            $(".gameMessages").text("Round " + round + " started!");
+            generateQuiz();
+            refreshData();
+            canPlay = false;
+            $("#play").prop("disabled", true);
+            $(".gameControl").addClass("panel-success");
+            $(".gameControl").removeClass("panel-danger");
+            $(".gameControl").removeClass("panel-warning");
+            
+
+        }
+
+        else if(canPlay && round >= 10){
+            $(".gameControl").addClass("panel-success");
+            $(".gameControl").removeClass("panel-danger");
+            $(".gameControl").removeClass("panel-warning");
+            $(".gameMessages").text("You've completed all 10 rounds for a final score of " + points + "!");
+        }
+
+        else $(".gameMessages").text("Round " + round + ": You're already playing!");   
+    });
+
+    $("#skip").on("click", function(){
+        if(round >= 10){
+            $(".gameControl").addClass("panel-success");
+            $(".gameControl").removeClass("panel-danger");
+            $(".gameControl").removeClass("panel-warning");
+            $(".gameMessages").text("You've completed all 10 rounds for a final score of " + points + "!");
+        }
+        else if(skips>0 && canPlay === false && round<10){
+
+            clearInterval(time);
+            timer();
+            skips--;
+            generateQuiz();
+            refreshData();
+            canPlay = false;
+            $("#play").prop("disabled", true);
+            $(".gameControl").removeClass("panel-success");
+            $(".gameControl").removeClass("panel-danger");
+            $(".gameControl").addClass("panel-warning");
+            $(".gameMessages").text("Round " + round + ": You have skipped a question. Skips left " + skips +".");
+        }
+
+        else if(skips>0 && canPlay === true){
+            $(".gameControl").removeClass("panel-success");
+            $(".gameControl").removeClass("panel-danger");
+            $(".gameControl").addClass("panel-warning");
+            $(".gameMessages").text("Round " + round + ": You have yet to start another turn, no need to waste skips!");
+
+        }
+        else {
+            $(".gameControl").removeClass("panel-success");
+            $(".gameControl").removeClass("panel-warning");
+            $(".gameControl").addClass("panel-danger");
+            $(".gameMessages").text("Round " + round + ": You're out of skips!");
+            //console.log(""); 
+            $("#skip").prop("disabled", true);
+        }
+    });
+
+    $("#reset").on("click", function(){
+
+        reset();
+    });
+
+    $(".btn-primary").on("click", function(){
+        
+        
+        if(this.id===String("b"+randomSet[3]) && canPlay===false) {
             $(this).text("Correct!").css("background-color","lightgreen");
             wins++;
-            if(difficulty==="easy") points += 1;
-            if(difficulty==="medium") points += 2;
-            if(difficulty==="hard") points += 3;
+            if(difficulty==="easy") { points += 1; }
+            if(difficulty==="medium") { points += 2; }
+            if(difficulty==="hard") { points += 3; }
+            
+            canPlay = true;
+            clearInterval(time);
 
-            generateQuiz();
-            $("#ws").text(wins);
-            $("#points").text(points);
-            $("#curdif").text(difficulty);
+            $("#play").prop("disabled", false);
+            
+            $(".gameControl").removeClass("panel-info");
+            $(".gameControl").removeClass("panel-warning");
+            $(".gameControl").addClass("panel-success");
+            $(".gameMessages").text("Round " + round + ": Correct!");
+            refreshData();
+            
         }
-        
-        else if(this.id!==String("b"+randomSet[3])) {
+
+        else if(this.id!==String("b"+randomSet[3]) && canPlay===false) {
             $(this).text("Incorrect!").css("background-color","red");
             losses++;
-            if(difficulty==="easy") points -= 1;
-            if(difficulty==="medium") points -= 2;
-            if(difficulty==="hard") points -= 3;
+            if(difficulty==="easy") { points -= 1; styleColor= "green";}
+            if(difficulty==="medium") { points -= 2; styleColor= "yellow";}
+            if(difficulty==="hard") { points -= 3; styleColor= "red";}
+            
+            canPlay = true;
+            clearInterval(time);
+            $("#play").prop("disabled", false);
+            
+            $(".gameControl").removeClass("panel-success");
+            $(".gameControl").removeClass("panel-info");
+            $(".gameControl").addClass("panel-warning");
 
-            generateQuiz();
-            $("#ls").text(losses);
-            $("#points").text(points);
-            $("#curdif").text(difficulty);
+            var temp = "#a" + randomSet[3];
+            //$(temp).text(response.results[0].correct_answer);
+            $(".gameMessages").text("Round " + round + ": Incorrect! The answer was " + $(temp).text());
+            refreshData();
         }
 
     });
